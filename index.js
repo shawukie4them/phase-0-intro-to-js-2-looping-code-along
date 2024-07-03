91c052730e6495031a842eb345398f11477e4475
shawukie4them <shawukiekawuki@gmail.com>
2024-07-03 12:45:13 +0300

    looping

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+//Exerise 1
+function writeCards(names, event){
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+//Exercise 2
+function countDown(number){
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
